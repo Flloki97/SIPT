8ff64e33798e7b9e4f2f7af2140a914231153917
flloki97 <fbryma7@gmail.com>
2025-02-17 23:10:34 +0100

    next  channel

--- scraper.js
+++ scraper.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 chromium.use(stealth());
 
 const urlsToScrape = [
-    'https://www.bbvipfalas.com/2025/01/blog-post.html' // First source
+    'https://bbvipal.net/' // First source
     // 'https://bigbrothervipalbania4.website/shiko-big-brother-vip-albania-4-live-kanali-2/'  // Replace with second source URL
 ];
 
