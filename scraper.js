ee73ee869d812df11413526e221f9508ff4527ef
flloki97 <fbryma7@gmail.com>
2025-02-23 16:36:14 +0100

    update

--- scraper.js
+++ scraper.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 chromium.use(stealth());
 
 const urlsToScrape = [
-    'https://bigbrothervipalbania4.website/shiko-big-brother-vip-albania-4-live-kanali-1/' // First source
+    'https://www.shikobbshqip.com/p/kanali-1-live-big-brother-albania-vip-3.html' // First source
     // 'https://bigbrothervipalbania4.website/shiko-big-brother-vip-albania-4-live-kanali-2/'  // Replace with second source URL
 ];
 
